6bae6681b5261bb6c093d22527400b9f9a4e94ef
luannc2000 <109270919+luannc2000@users.noreply.github.com>
2022-07-14 15:58:48 +0700

    Delete pokemon.ts

--- api/pokemon.ts
+++
@@ -1,8 +0,0 @@
-import instance from "./instance"
-
-export const getAll = () =>{
-    return instance.get("/pokemons")
-}
-export const get = (id) =>{
-    return instance.get(`/pokemons/${id}`)
-}
